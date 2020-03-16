db0c50f02dac51425fae3789ed192f6375e778de
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-03-16 15:48:10 -0600

    yoo

--- tests/header.test.js
+++ tests/header.test.js
@@ -14,8 +14,16 @@ afterEach(async () => {
   await browser.close();
 });
 
-test('We can launch a browser', async () => {
+test('The header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
   expect(text).toEqual('Blogster');
+});
+
+test('Clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
 });
