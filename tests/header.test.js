5c484cada8d134a27d7b0f2c63fb2c53b6675dbb
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-03-17 16:00:24 -0600

    Combining the page and browser

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 
 let browser, page;
 
@@ -26,4 +28,18 @@ test('Clicking login starts oauth flow', async () => {
   const url = await page.url();
 
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  const user = await userFactory();
+  const { session, sig } = sessionFactory(user);
+
+  await page.setCookie({ name: 'session', value: session });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
 });
