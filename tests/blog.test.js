011ebaf0364f8025a61a3c63e2a0c7ab1000ada9
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-03-29 15:34:20 -0600

    Asserting Validation errors

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -22,4 +22,18 @@ describe('When logged in', async () => {
 
     expect(label).toEqual('Blog Title');
   });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+    test('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
 });
