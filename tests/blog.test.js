b271b2ff45ea117ef0c9121a8c4b40d785711ce1
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-03-29 16:25:58 -0600

    Asserting page Response

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -61,3 +61,20 @@ describe('When logged in', async () => {
     });
   });
 });
+
+describe('User is not logged in', async () => {
+  test('User cannot create blog posts', async () => {
+    const result = await page.evaluate(() => {
+      return fetch('/api/blogs', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: 'My Title', content: 'My Content' })
+      }).then(res => res.json());
+    });
+
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
+});
