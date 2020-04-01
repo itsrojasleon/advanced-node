85862571000341929e92f1921a8e0929913c00d5
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-03-31 18:24:44 -0600

    Let's start with ci

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -63,18 +63,26 @@ describe('When logged in', async () => {
 });
 
 describe('User is not logged in', async () => {
-  test('User cannot create blog posts', async () => {
-    const result = await page.evaluate(() => {
-      return fetch('/api/blogs', {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ title: 'My Title', content: 'My Content' })
-      }).then(res => res.json());
-    });
+  const actions = [
+    {
+      method: 'get',
+      path: '/api/blogs'
+    },
+    {
+      method: 'post',
+      path: '/api/blogs',
+      data: {
+        title: 'T',
+        content: 'C'
+      }
+    }
+  ];
 
-    expect(result).toEqual({ error: 'You must log in!' });
+  test('Blog related actions are prohibited', async () => {
+    const results = await page.execRequests(actions);
+
+    for (let result of results) {
+      expect(result).toEqual({ error: 'You must log in!' });
+    }
   });
 });
