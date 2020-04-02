f9c305d802ba045176467f7c42e01ca12f79fa82
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-04-01 18:06:23 -0600

    ci

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
+const keys = require('../config/keys');
 
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
+const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
@@ -12,13 +12,13 @@ mongoose.Query.prototype.cache = function(options = {}) {
   this.hashKey = JSON.stringify(options.key || '');
 
   return this;
-}
+};
 
 mongoose.Query.prototype.exec = async function() {
   if (!this.useCache) {
     return exec.apply(this, arguments);
   }
-  
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
@@ -30,20 +30,20 @@ mongoose.Query.prototype.exec = async function() {
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
 
-    return Array.isArray(doc) 
+    return Array.isArray(doc)
       ? doc.map(d => new this.model(d))
       : new this.model(doc);
   }
-  
+
   const result = await exec.apply(this, arguments);
 
   client.set(key, JSON.stringify(result), 'EX', 10);
 
   return result;
-}
+};
 
 module.exports = {
   clearHash(hashKey) {
     client.del(JSON.stringify(hashKey));
   }
-}
+};
